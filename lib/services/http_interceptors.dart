5acc7dc585ec5975cd9dc977d839e19754b5afe8
theotrin <theopinheiro12@gmail.com>
2024-12-10 18:18:05 -0400

    feat: adcionado logger

--- lib/services/http_interceptors.dart
+++ lib/services/http_interceptors.dart
@@ -1,13 +1,15 @@
 import 'package:http_interceptor/http_interceptor.dart';
+import 'package:logger/logger.dart';
 
 class LoggerInterceptor extends InterceptorContract {
+  Logger logger = Logger();
+
   @override
   Future<BaseRequest> interceptRequest({
     required BaseRequest request,
   }) async {
-    print('----- Request -----');
-    print(request.toString());
-    print(request.headers.toString());
+    logger.v('Request for: ${request.url}');
+
     return request;
   }
 
@@ -15,10 +17,9 @@ class LoggerInterceptor extends InterceptorContract {
   Future<BaseResponse> interceptResponse({
     required BaseResponse response,
   }) async {
-    print('----- Response -----');
-    print('Code: ${response.statusCode}');
+    logger.i('Code: ${response.statusCode}');
     if (response is Response) {
-      print((response).body);
+      logger.v((response).headers);
     }
     return response;
   }
